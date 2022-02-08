89bb0807ee91ea8e5a7ae936e07a7e45c17779ed
gachez <briangach98@gmail.com>
2022-02-08 21:41:00 +0300

    change labels to be placeholders

--- src/pages/Academics/CourseCohorts.tsx
+++ src/pages/Academics/CourseCohorts.tsx
@@ -1,37 +1,37 @@
 /* eslint-disable react/display-name */
-import React,{useState,useEffect} from 'react';
-import {forwardRef} from 'react';
+import React,{useState,useEffect} from 'react'
+import {forwardRef} from 'react'
 
-import MaterialTable, { MTableToolbar } from 'material-table';
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import { Icons } from 'material-table';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
-import axios from 'axios';
-import Card from '@material-ui/core/Card';
-import Alert from '@material-ui/lab/Alert';
-import Breadcrumb from '../../App/components/Breadcrumb';
-import {Row,Col,Modal,Button} from 'react-bootstrap';
-import Config from '../../config';
-import {MenuItem, Select, Switch} from '@material-ui/core';
-import {ValidationForm,SelectGroup,FileInput,TextInput} from 'react-bootstrap4-form-validation';
-import CardPreview from './CardPreview';
-import {Link} from 'react-router-dom';
-import { Alerts, ToastifyAlerts } from '../lib/Alert';
-import { LinearProgress } from '@mui/material';
-const alerts: Alerts = new ToastifyAlerts();
+import MaterialTable, { MTableToolbar } from 'material-table'
+import AddBox from '@material-ui/icons/AddBox'
+import ArrowDownward from '@material-ui/icons/ArrowDownward'
+import Check from '@material-ui/icons/Check'
+import ChevronLeft from '@material-ui/icons/ChevronLeft'
+import ChevronRight from '@material-ui/icons/ChevronRight'
+import Clear from '@material-ui/icons/Clear'
+import DeleteOutline from '@material-ui/icons/DeleteOutline'
+import Edit from '@material-ui/icons/Edit'
+import { Icons } from 'material-table'
+import FilterList from '@material-ui/icons/FilterList'
+import FirstPage from '@material-ui/icons/FirstPage'
+import LastPage from '@material-ui/icons/LastPage'
+import Remove from '@material-ui/icons/Remove'
+import SaveAlt from '@material-ui/icons/SaveAlt'
+import Search from '@material-ui/icons/Search'
+import ViewColumn from '@material-ui/icons/ViewColumn'
+import axios from 'axios'
+import Card from '@material-ui/core/Card'
+import Alert from '@material-ui/lab/Alert'
+import Breadcrumb from '../../App/components/Breadcrumb'
+import {Row,Col,Modal,Button} from 'react-bootstrap'
+import Config from '../../config'
+import {MenuItem, Select, Switch} from '@material-ui/core'
+import {ValidationForm,SelectGroup,FileInput,TextInput} from 'react-bootstrap4-form-validation'
+import CardPreview from './CardPreview'
+import {Link} from 'react-router-dom'
+import { Alerts, ToastifyAlerts } from '../lib/Alert'
+import { LinearProgress } from '@mui/material'
+const alerts: Alerts = new ToastifyAlerts()
 const tableIcons: Icons = {
     Add: forwardRef((props, ref) => < AddBox  {...props} ref={ref} />),
     Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
@@ -50,35 +50,35 @@ const tableIcons: Icons = {
     SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
     ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
     ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-};
+}
 const CourseCohorts = ():JSX.Element => {
   
 
-    const [data,setData]=useState([]);
-    const [trainersData,setTrainers] = useState([]);
-    const [semesters,setSemesters] = useState([]);
-    const [isError]=useState(false);
-    const [,setDisabled]=useState(false);
-    const [programId,setProgramId]=useState(0);
-    const [campusId,setCampusId]=useState(0);
-    const [startDate,setStartDate]=useState('');
-    const [banner,setBanner]=useState('');
-    const [graduationDate,setGraduationDate]=useState('');
-    const [description,setDescription]=useState('');
-    const [imageUploaded,setImageUploaded]=useState('');
-    const [programs,setPrograms]=useState([]);
-    const [campus,setCampus]=useState([]);
-    const [programName,setProgramName]=useState('');
-    const [selectedCampusId,setSelectedCampusId]=useState(0);
-    const [selectedProgramId,setSelectedProgramId]=useState(0);
-    const [selectedGraduationDate]=useState();
-    const [selectedStartDate]=useState();
-    const [selectedDescription]=useState();
-    const [showModal,setModal]=useState(false);
-    const [cohortId,setCohortId]=useState(null);
-    const [errorMessages]=useState([]);
-    const [linearDisplay, setLinearDisplay] = useState('none');
-    const timetablingSrv=Config.baseUrl.timetablingSrv;
+    const [data,setData]=useState([])
+    const [trainersData,setTrainers] = useState([])
+    const [semesters,setSemesters] = useState([])
+    const [isError]=useState(false)
+    const [,setDisabled]=useState(false)
+    const [programId,setProgramId]=useState(0)
+    const [campusId,setCampusId]=useState(0)
+    const [startDate,setStartDate]=useState('')
+    const [banner,setBanner]=useState('')
+    const [graduationDate,setGraduationDate]=useState('')
+    const [description,setDescription]=useState('')
+    const [imageUploaded,setImageUploaded]=useState('')
+    const [programs,setPrograms]=useState([])
+    const [campus,setCampus]=useState([])
+    const [programName,setProgramName]=useState('')
+    const [selectedCampusId,setSelectedCampusId]=useState(0)
+    const [selectedProgramId,setSelectedProgramId]=useState(0)
+    const [selectedGraduationDate]=useState()
+    const [selectedStartDate]=useState()
+    const [selectedDescription]=useState()
+    const [showModal,setModal]=useState(false)
+    const [cohortId,setCohortId]=useState(null)
+    const [errorMessages]=useState([])
+    const [linearDisplay, setLinearDisplay] = useState('none')
+    const timetablingSrv=Config.baseUrl.timetablingSrv
    
    
 
@@ -89,44 +89,44 @@ const CourseCohorts = ():JSX.Element => {
         {
             title: 'Actions',
             field: 'internal_action',
-            render: (row) => (
+            render: (row) => 
                 <Select>
                     <Link to='/coursecohortdetails'>
                         <MenuItem value="View courses">View details</MenuItem>
                     </Link>
                 </Select>
-            )
+            
         }
-    ];
+    ]
     useEffect(()=>{
-        setLinearDisplay('block');
+        setLinearDisplay('block')
         fetchcourseCohorts()
         fetchTrainers()
         fetchSemesters()
-    },[]);
+    },[])
     const fetchcourseCohorts = ():void => {
         axios.get(`${timetablingSrv}/course-cohorts`, {params:{loadExtras:'trainer' }})
-        .then(res => {
-            const ccData = res.data;
-            setData(ccData);
-            setLinearDisplay('none');
-        });
+            .then(res => {
+                const ccData = res.data
+                setData(ccData)
+                setLinearDisplay('none')
+            })
     }
 
     const fetchTrainers = ():void => {
         axios.get(`${timetablingSrv}/trainers`)
-        .then(res => {
-            const trData = res.data;
-            setTrainers(trData);
-        });
+            .then(res => {
+                const trData = res.data
+                setTrainers(trData)
+            })
     }
 
     const fetchSemesters = ():void => {
         axios.get(`${timetablingSrv}/semesters`)
-        .then(res => {
-            const semData = res.data;
-            setSemesters(semData)
-        });
+            .then(res => {
+                const semData = res.data
+                setSemesters(semData)
+            })
     }
     return (
         <>
@@ -143,7 +143,7 @@ const CourseCohorts = ():JSX.Element => {
                             {isError&&
                             <Alert severity='error'>
                                 {errorMessages.map((msg,i)=>{
-                                    return <div key={i}>{msg}</div>;
+                                    return <div key={i}>{msg}</div>
                                 })}
                             </Alert>
                             }
@@ -155,49 +155,50 @@ const CourseCohorts = ():JSX.Element => {
                             data={data}
                             options={{actionsColumnIndex:-1}}
                             components={{
-                                Toolbar: props => (
+                                Toolbar: props => 
                                     <div>
                                         <MTableToolbar {...props} />
                                         <div style={{padding: '0px 24px'}}>
-                                            <label >Filter by trainer: </label>
                                             <Select
                                                 labelId="demo-simple-select-label"
                                                 id="demo-simple-select"
+                                                label="Trainer"
                                                 style={{width:150}}
                                             >
-                                            {
-                                                trainersData.map(tr => {
-                                                    return (
-                                                        <MenuItem value={tr.tr_id}>{tr.tr_id}</MenuItem>
-                                                    )
-                                                })
-                                            }
+                                                {
+                                                    trainersData.map(tr => {
+                                                        return (
+                                                            <MenuItem value={tr.tr_id}>{tr.tr_id}</MenuItem>
+                                                        )
+                                                    })
+                                                }
                                             </Select>
 
-                                            <label style={{marginLeft: 48}}>Filter by semester: </label>
+                                            <label style={{marginLeft: 48}}>Semester: </label>
                                             <Select
                                                 labelId="demo-simple-select-label"
                                                 id="demo-simple-select"
+                                                label="Semester"
                                                 style={{width:150}}
                                             >
-                                            {
-                                                semesters.map(sem => {
-                                                    return (
-                                                        <MenuItem value={sem.id}>{sem.name}</MenuItem>
-                                                    )
-                                                })
-                                            }
+                                                {
+                                                    semesters.map(sem => {
+                                                        return (
+                                                            <MenuItem value={sem.id}>{sem.name}</MenuItem>
+                                                        )
+                                                    })
+                                                }
                                             </Select>
                                         </div>
                                     </div>
-                                ),
+                                
                             }}
                         />
                     </Card>
                 </Col>
             </Row>
         </>
-    );
-};
+    )
+}
 
-export default CourseCohorts;
+export default CourseCohorts
