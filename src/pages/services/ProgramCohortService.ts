34e3150a8a5f7d288dc823bd9356a6808ef51a01
David Kiragu <davidkmuriba@gmail.com>
2022-02-15 12:13:55 +0300

    Added cancel button on program cohort ui added cancel modal
    Signed-off-by: David Kiragu <davidkmuriba@gmail.com>

--- src/pages/services/ProgramCohortService.ts
+++ src/pages/services/ProgramCohortService.ts
@@ -2,9 +2,13 @@ import axios from 'axios'
 import Config from '../../config'
 import FeeItem from './FeeItem'
 const financeSrv = Config.baseUrl.financeSrv
+const timetablingSrv=Config.baseUrl.timetablingSrv
 export class ProgramCohortService {
-	static  async publishProgramCohortSemester (programCohortSemesterId: string, programCohortSemester: object): Promise<FeeItem[]> {
+	static  async publishProgramCohortSemester (programCohortSemesterId: string, programCohortSemester: object): Promise<void> {
 		return axios.put(`${financeSrv}/program-cohort-semesters/${programCohortSemesterId}`,{ ModifyProgramCohortSemesterRequest: programCohortSemester})
+	}
+	static  async cancelProgramCohort (cohortId: number, cancelletionData: object): Promise<void> {
+		return axios.put(`${timetablingSrv}/program-cohorts/${cohortId}`, cancelletionData)
 	}
 
 }
