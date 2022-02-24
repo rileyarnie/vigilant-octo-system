c97ec92faec727364f78cb0f070ab6aa7b950f5a
gachez <briangach98@gmail.com>
2022-02-24 07:38:17 +0300

    fix edit timetable unit

--- src/pages/Timetabling/Timetable.jsx
+++ src/pages/Timetabling/Timetable.jsx
@@ -150,8 +150,8 @@ class Timetable extends React.Component {
 
 timeTabledUnitsWithErrors (timeTabledUnits,timeTabledUnitErrors) {
 const items = timeTabledUnits.map(unit=>({
-...timeTabledUnitErrors.find((error)=>(error.timetablingUnitId===unit.timetablingUnitId)&&error),priorityId: 2
-,...unit
+...timeTabledUnitErrors?.find((error)=>(error.timetablingUnitId===unit.timetablingUnitId)&&error),priorityId: 2,
+...unit
 }))
  const itemsWithColor=[]
 // const itemsWithColor=items.map(item=>item.errors.length>0?({...item,item.color:"#ff0000"}):({{...item,item.color:"#000ff0"}})}
@@ -203,13 +203,14 @@ const items = timeTabledUnits.map(unit=>({
         const min =  Math.min(timetableData.trainingHours, this.state.maxNumUnitRepetition)
         const timetableUnit = {
             courseCohortId: timetableData.id,
-            recurrenceStartDate: timetableData.startDate,
-            recurrenceEndDate: timetableData.endDate,
+            recurrenceStartDate: new Date(timetableData.startDate),
+            recurrenceEndDate: new Date(timetableData.endDate),
             startTime: timetableData.startDate.toTimeString().slice(0,8),
             numSessions: min || 1,
             durationInMinutes: 60,
             colorId: this.state.colorId,
         }
+        console.log(timetableUnit)
         TimetableService.createTimetableUnit(timetableUnit)
             .then(() => {
                 alerts.showSuccess('TimetableUnit added successfully')
