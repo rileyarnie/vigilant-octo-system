165c8f2970fb8cfa5fc4296e698d631ca9767e73
Arnold Kibira <arnold.ril3y@gmail.com>
2022-02-25 10:14:55 +0300

    Create fee waiver component

---
+++ src/pages/Academics/FeeWaiver.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Button, Col, Form, FormControl, Row } from 'react-bootstrap';
+import Breadcrumb from '../../App/components/Breadcrumb';
+import ModalWrapper from '../../App/components/modal/ModalWrapper';
+
+
+interface Props {
+    show: boolean;
+    closeModal: () => void;
+}
+
+
+const FeeWaiver:React.FunctionComponent<Props> = (props) => {
+
+
+    //submit function to come here
+    const handleSubmit = () => {
+        console.log('Submitted');
+    };
+    return (
+        <div>FeeWaiver
+            <div>
+                <ModalWrapper
+                    show={props.show}
+                    closeModal={props.closeModal}
+                    title="Upload Fee Waiver"
+                    modalSize="lg"
+                    submitButton
+                    submitFunction={handleSubmit}
+                >
+                    <Form>
+                        <Form.Group controlId="formAmmount">
+                            <Row style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                <Col sm={3}>Amount:</Col>
+                                <Col sm={9}>
+                                    <FormControl type="number" placeholder="Enter Amount" />
+                                </Col>
+                            </Row>
+                        </Form.Group>
+                        <Form.Group controlId="formAmmount">
+                            <Row style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                <Col sm={3}>Narrative:</Col>
+                                <Col sm={9}>
+                                    <FormControl type="text" as="textarea" placeholder="Enter Narrative" />
+                                </Col>
+                            </Row>
+                        </Form.Group>
+                        <Form.Group controlId="formAmmount">
+                            <Row style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                <Col sm={3}>Supporting Documents:</Col>
+                                <Col sm={9}>
+                                    <FormControl type="file" placeholder="Enter Amount" />
+                                </Col>
+                            </Row>
+                        </Form.Group>
+                        <Form.Group controlId="formAmmount">
+                            <Row style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                <Col sm={3}>Invoice Number:</Col>
+                                <Col sm={9}>
+                                    <FormControl type="select" as="select" placeholder="Enter Invoice Number" />
+                                </Col>
+                            </Row>
+                        </Form.Group>
+                    </Form>
+                </ModalWrapper>
+            </div></div>
+    );
+};
+
+export default FeeWaiver;
