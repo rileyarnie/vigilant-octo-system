ea286c3e4ebfda6dfd1e6fb50e33a9d5360b5a1a
Davidkiragu <davidkmuriba@gmail.com>
2022-03-17 01:24:24 +0300

    Added power bi client

--- src/pages/Dashboard/Dasboard1.tsx
+++ src/pages/Dashboard/Dasboard1.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
 import { Row, Col, Card } from 'react-bootstrap';
+import { PowerBIEmbed } from 'powerbi-client-react';
+import { models, Report, Embed, service, Page } from 'powerbi-client';
 function Dashboard1() {
     return (
         <>
             <Row>
                 <Col>
                     <Card>
-                        {/*iframe comes here*/}
+                        <PowerBIEmbed
+                            embedConfig = {{
+                                type: 'report',   // Supported types: report, dashboard, tile, visual and qna
+                                id: '<Report Id>',
+                                embedUrl: '<Embed Url>',
+                                accessToken: '<Access Token>',
+                                tokenType: models.TokenType.Embed,
+                                settings: {
+                                    panes: {
+                                        filters: {
+                                            expanded: false,
+                                            visible: false
+                                        }
+                                    },
+                                    background: models.BackgroundType.Transparent,
+                                }
+                            }}
+
+                            eventHandlers = {
+                                new Map([
+                                    ['loaded', function () {console.log('Report loaded');}],
+                                    ['rendered', function () {console.log('Report rendered');}],
+                                    ['error', function (event) {console.log(event.detail);}]
+                                ])
+                            }
+
+                            cssClassName = {
+                                'report-style-class'
+                            }
+                            // getEmbeddedComponent = {
+                            //     (embeddedReport) => {
+                            //         windw.report = embeddedReport;
+                            //     }
+                            // }
+                        />
                     </Card>
                 </Col>
             </Row>
