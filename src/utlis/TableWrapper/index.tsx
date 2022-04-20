fbb6c2ba9016f5450abff56182a0e7954b77b7bc
Arnold Kibira <arnold.ril3y@gmail.com>
2022-04-20 21:15:41 +0300

    Create higher order table component to vaoid code duplication and default styles set on the wrapper component

---
+++ src/utlis/TableWrapper/index.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/display-name */
+import React from 'react';
+import MaterialTable, { Icons } from 'material-table';
+import { forwardRef } from 'react';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons: Icons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
+
+// eslint-disable-next-line react/prop-types
+const TableWrapper = ({ title, data, columns, options, ...props }): JSX.Element => {
+    return (
+        <MaterialTable
+            title={title}
+            data={data}
+            columns={columns}
+            icons={tableIcons}
+            options={{ ...options, pageSize: 50, headerStyle: { backgroundColor: '#242e3e', color: '#b5bdca', fontWeight: 'bold' } }}
+            {...props}
+        />
+    );
+};
+
+export default TableWrapper;
