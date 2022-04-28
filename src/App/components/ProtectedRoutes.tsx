56b43f4a22a27d4ec56ea616337c4c6c3565b313
tomogoma <ogomatom@gmail.com>
2022-04-28 17:04:35 +0300

    Fix redirection not using basename

--- src/App/components/ProtectedRoutes.tsx
+++ src/App/components/ProtectedRoutes.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
 import { Route, Redirect, RouteProps } from 'react-router-dom';
 import React from 'react';
+import Config from '../../config';
 
 interface Props extends RouteProps {
     isLoggedIn: boolean;
 }
 
 export const ProtectedRoutes: React.FC<Props> = ({ component: Component, isLoggedIn, ...rest }) => (
-    <Route {...rest} render={(props) => (isLoggedIn === true ? <Component {...props} /> : <Redirect to="/login" />)} />
+    <Route {...rest} render={(props) => (isLoggedIn === true ? <Component {...props} /> : <Redirect to={Config.basename+"/login"} />)} />
 );
