a0e6e078879c0f7a34f3679c9c5d79748fba7cfa
Arnold Kibira <arnold.ril3y@gmail.com>
2022-05-12 08:42:18 +0000

    Merged in feature/SILMS-1489-as-a-staff-user-i-would-like- (pull request #208)
    Allow user to view all actions assigned to a role without having to edit
    
    * refactor code to move set linear display to the finally block
    
    add a view actions button on the dropdown to allow users to view actions in a role without editing
    
    add paramenter to fetch roles function which toggles modals only when request is successfull …
    
    Approved-by: David Kiragu

--- src/pages/Users/RoleList.tsx
+++ src/pages/Users/RoleList.tsx
@@ -1,27 +1,29 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Alert from '@material-ui/lab/Alert';
-import {Button, Card, Col, Modal, Row} from 'react-bootstrap';
+import { Button, Card, Col, Modal, Row } from 'react-bootstrap';
 import Breadcrumb from '../../App/components/Breadcrumb';
-import {Actions} from './ActionsByRole/Actions';
-import {AddActions} from './AddActionsModal/AddActions';
+import { Actions } from './ActionsByRole/Actions';
+import { AddActions } from './AddActionsModal/AddActions';
 import CreateRole from './Role/CreateRole';
-import {Alerts, ToastifyAlerts} from '../lib/Alert';
-import {LinearProgress} from '@mui/material';
-import {MenuItem, Select} from '@material-ui/core';
-import {VerticalModal} from './ActionsByRole/VerticalModal';
-import {AddActionsModal} from './AddActionsModal/AddActionsModal';
-import {canPerformActions} from '../../services/ActionChecker';
+import { Alerts, ToastifyAlerts } from '../lib/Alert';
+import { LinearProgress } from '@mui/material';
+import { MenuItem, Select } from '@material-ui/core';
+import { VerticalModal } from './ActionsByRole/VerticalModal';
+import { AddActionsModal } from './AddActionsModal/AddActionsModal';
+import { canPerformActions } from '../../services/ActionChecker';
 import {
     ACTION_ADD_ACTIONS_TO_ROLE,
     ACTION_DEACTIVATE_ROLE,
+    ACTION_GET_ACTIONS_BY_ROLE_ID,
     ACTION_GET_ROLES
 } from '../../authnz-library/authnz-actions';
-import {authnzAxiosInstance} from '../../utlis/interceptors/authnz-interceptor';
+import { authnzAxiosInstance } from '../../utlis/interceptors/authnz-interceptor';
 import TableWrapper from '../../utlis/TableWrapper';
 import ConfirmationModalWrapper from '../../App/components/modal/ConfirmationModalWrapper';
 import CustomSwitch from '../../assets/switch/CustomSwitch';
+import ModalWrapper from '../../App/components/modal/ModalWrapper';
 
 const alerts: Alerts = new ToastifyAlerts();
 
@@ -32,85 +34,10 @@ interface Role {
     RoleName: string;
 }
 function roleList(): JSX.Element {
-    const [switchStatus,setSwitchStatus] = useState<boolean>();
+    const [switchStatus, setSwitchStatus] = useState<boolean>();
     const [activationModal, setActivationModal] = useState(false);
-    const [selectedRow,setselectedRow] = useState<{name:string,id:number}>();
+    const [selectedRow, setselectedRow] = useState<{ name: string; id: number }>();
     const [disabled, setDisabled] = useState(false);
-    const columns = [
-        { title: 'ID', field: 'id', editable: 'never' as const },
-        { title: 'Role Name', field: 'name', editable: 'always' as const },
-        { title: 'Role Description', field: 'description', editable: 'always' as const },
-        { title: 'Activation Status', field: 'activationStatus', editable: 'never' as const },
-        { title: 'Created On', render: (row: Role): string => row.createdOn?.slice(0,10), editable: 'never' as const },
-        {
-            title: ' Actions',
-            render: (row: Role) => (
-                <Select>
-                    {canPerformActions(ACTION_ADD_ACTIONS_TO_ROLE.name) && (
-                        <div
-                            className=""
-                            onClick={() => {
-                                setId(row.id);
-                                setRoleName(row.name);
-                                getActionsByRoleId(row.id, row.name);
-                            }}
-                        >
-                            <MenuItem value="Add actions">Add Actions</MenuItem>
-                        </div>
-                    )}
-
-                    {canPerformActions(ACTION_DEACTIVATE_ROLE.name) && (
-                        <div
-                            className=""
-                            onClick={() => {
-                                setId(row.id);
-                                setRoleName(row.name);
-                                handleRowDelete(row.id);
-                            }}
-                        >
-                            <MenuItem value="delete role">Delete Role</MenuItem>
-                        </div>
-                    )}
-                </Select>
-            )
-        },
-        {
-            title: 'Activation Status',
-            field: 'internal_action',
-            render: (row) =>
-                (
-                    <>
-                        <CustomSwitch
-                            defaultChecked={row.activationStatus}
-                            color="secondary"
-                            inputProps={{'aria-label': 'controlled'}}
-                            checked={row.activationStatus}
-                            onChange={(event) => {
-                                setselectedRow(row);
-                                setActivationModal(true);
-                                setSwitchStatus(event.target.checked);
-                                
-                            }}
-                        />
-                        <ConfirmationModalWrapper
-                            disabled={disabled}
-                            submitButton
-                            submitFunction={() => updateRole(selectedRow?.id,{activationStatus:switchStatus})}
-                            closeModal={handleCloseModal}
-                            show={activationModal}
-                        >
-                            <h6 className="text-center">
-                                Are you sure you want to change the status of <>{selectedRow?.name}</> ?
-                            </h6>
-                        </ConfirmationModalWrapper>
-                    </>
-                )
-        }
-    ];
-    const actionColumns = [
-        { title: 'id', field: 'id' },
-        { title: 'Action Name', field: 'name' }
-    ];
     const [data, setData] = useState([]);
     const [id, setId] = useState(0);
     const [roleName, setRoleName] = useState('');
@@ -122,7 +49,95 @@ function roleList(): JSX.Element {
     const [defaultRoleValues, setDefaultRoleValues] = useState([]);
     //modal functions
     const [verticalModal, setVerticalModal] = React.useState(false);
-    const [actionModal, setActionModal] = React.useState(false);
+    const [actionListModal, setActionListModal] = useState(false);
+    const [addActionsModal, setAddActionsModal] = useState(false);
+
+    const columns = [
+        { title: 'ID', field: 'id', editable: 'never' as const },
+        { title: 'Role Name', field: 'name', editable: 'always' as const },
+        { title: 'Role Description', field: 'description', editable: 'always' as const },
+        { title: 'Activation Status', field: 'activationStatus', editable: 'never' as const },
+        { title: 'Created On', render: (row: Role): string => row.createdOn?.slice(0, 10), editable: 'never' as const },
+        {
+            title: ' Actions',
+            render: (row: Role) => (
+                <Select>
+                    {canPerformActions(ACTION_ADD_ACTIONS_TO_ROLE.name) && (
+                        <div
+                            className=""
+                            onClick={() => {
+                                setId(row.id);
+                                setRoleName(row.name);
+                                getActionsByRoleId(row.id, row.name, 'addActions');
+                            }}
+                        >
+                            <MenuItem value="Add actions">Add Actions</MenuItem>
+                        </div>
+                    )}
+                    {canPerformActions(ACTION_GET_ACTIONS_BY_ROLE_ID.name) && (
+                        <div
+                            className=""
+                            onClick={() => {
+                                setId(row.id);
+                                setRoleName(row.name);
+                                getActionsByRoleId(row.id, row.name, 'viewActions');
+                            }}
+                        >
+                            <MenuItem value="Add actions">View Actions</MenuItem>
+                        </div>
+                    )}
+
+                    {canPerformActions(ACTION_DEACTIVATE_ROLE.name) && (
+                        <div
+                            className=""
+                            onClick={() => {
+                                setId(row.id);
+                                setRoleName(row.name);
+                                // handleRowDelete(row.id);
+                            }}
+                        >
+                            <MenuItem value="delete role">Delete Role</MenuItem>
+                        </div>
+                    )}
+                </Select>
+            )
+        },
+        {
+            title: 'Activation Status',
+            field: 'internal_action',
+            render: (row) => (
+                <>
+                    <CustomSwitch
+                        defaultChecked={row.activationStatus}
+                        color="secondary"
+                        inputProps={{ 'aria-label': 'controlled' }}
+                        checked={row.activationStatus}
+                        onChange={(event) => {
+                            setselectedRow(row);
+                            setActivationModal(true);
+                            setSwitchStatus(event.target.checked);
+                        }}
+                    />
+                    <ConfirmationModalWrapper
+                        disabled={disabled}
+                        submitButton
+                        submitFunction={() => updateRole(selectedRow?.id, { activationStatus: switchStatus })}
+                        closeModal={handleCloseModal}
+                        show={activationModal}
+                    >
+                        <h6 className="text-center">
+                            Are you sure you want to change the status of <>{selectedRow?.name}</> ?
+                        </h6>
+                    </ConfirmationModalWrapper>
+                </>
+            )
+        }
+    ];
+    const actionColumns = [
+        { title: 'id', field: 'id' },
+        { title: 'Action Name', field: 'name' }
+    ];
+
     useEffect(() => {
         fetchRoles();
     }, []);
@@ -134,14 +149,15 @@ function roleList(): JSX.Element {
     function fetchRoles(): void {
         setLinearDisplay('block');
         authnzAxiosInstance
-            .get('/roles', { params: { includeDeactivated: true }})
+            .get('/roles', { params: { includeDeactivated: true } })
             .then((res: { data: [] }) => {
                 setData(res.data);
-                setLinearDisplay('none');
             })
             .catch((error) => {
-                setLinearDisplay('none');
                 alerts.showError((error as Error).message);
+            })
+            .finally(() => {
+                setLinearDisplay('none');
             });
     }
     function handleRowDelete(id: number): void {
@@ -150,16 +166,18 @@ function roleList(): JSX.Element {
             .delete(`/roles/${id}`)
             .then(() => {
                 fetchRoles();
-                setLinearDisplay('none');
                 alerts.showSuccess('Successfully deleted role');
             })
             .catch((error) => {
                 console.error(error);
                 alerts.showError((error as Error).message);
+            })
+            .finally(() => {
+                setLinearDisplay('none');
             });
     }
 
-    const getActionsByRoleId = (roleId: number, roleName?: string) => {
+    const getActionsByRoleId = (roleId: number, roleName: string, type: string) => {
         setDefaultRoleValues([]);
         setLinearDisplay('block');
         authnzAxiosInstance
@@ -168,14 +186,13 @@ function roleList(): JSX.Element {
                 const myData = res.data;
                 setActions(myData);
                 setDefaultRoleValues(res.data.map((role: { id: number; name: string }) => ({ value: role.id, label: role.name })));
-                setLinearDisplay('none');
+                return type === 'viewActions' ? toggleActionListModal() : toggleAddActionsModal();
             })
             .catch((err) => {
-                alerts.showError(`We couldn’t fetch the existing actions for ${roleName}, reopening the dialog should resolve this`);
+                alerts.showError(`We couldn’t fetch the existing actions for ${roleName}, please try again`);
             })
-            .finally(()=>{
+            .finally(() => {
                 setLinearDisplay('none');
-                setActionModal(true);
             });
     };
 
@@ -184,15 +201,13 @@ function roleList(): JSX.Element {
         setLinearDisplay('block');
         authnzAxiosInstance
             .put(`/roles/${roleId}`, updates)
-            .then(() => {                
-                fetchRoles();                
-                setLinearDisplay('none');
+            .then(() => {
+                fetchRoles();
                 alerts.showSuccess('Successfully updated role');
             })
             .catch((error) => {
                 console.error(error);
                 alerts.showError(error.message);
-                
             })
             .finally(() => {
                 setLinearDisplay('none');
@@ -205,8 +220,11 @@ function roleList(): JSX.Element {
         id: id,
         name: roleName
     };
-    const toggleActionsModal = () => {
-        setActionModal(!actionModal);
+    const toggleActionListModal = () => {
+        setActionListModal(!actionListModal);
+    };
+    const toggleAddActionsModal = () => {
+        setAddActionsModal(!addActionsModal);
     };
     const viewRoleActions = () => {
         setViewActions(true);
@@ -248,39 +266,22 @@ function roleList(): JSX.Element {
                 &nbsp;&nbsp;&nbsp;
                 <AddActions {...selectedRowProps}> </AddActions>
             </div>
-            <VerticalModal show={verticalModal} onHide={() => setVerticalModal(false)} selectedrowprops={selectedRowProps} />
+            {/* <VerticalModal show={verticalModal} onHide={() => setVerticalModal(false)} selectedrowprops={selectedRowProps} /> */}
             <AddActionsModal
-                show={actionModal}
-                toggleModal={toggleActionsModal}
-                onHide={() => setActionModal(false)}
+                show={addActionsModal}
+                toggleModal={toggleAddActionsModal}
+                onHide={() => setAddActionsModal(!addActionsModal)}
                 selectedRowProps={selectedRowProps}
                 defaultValues={defaultRoleValues}
             />
-            <Modal
-                show={viewActions}
-                onHide={viewRoleActions}
-                size="lg"
-                backdrop="static"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
+            <ModalWrapper
+                modalSize="lg"
+                title={`Actions assigned to ${roleName} `}
+                show={actionListModal}
+                closeModal={toggleActionListModal}
             >
-                <Modal.Header>
-                    <Modal.Title id="contained-modal-title-vcenter"> Assign actions to {roleName} </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <TableWrapper
-                        title="Action Lists"
-                        columns={actionColumns}
-                        data={actions}
-                        options={{ pageSize: 10 }}
-                    />
-                </Modal.Body>
-                <Modal.Footer style={{ display: 'flex', justifyContent: 'flex-start' }}>
-                    <Button variant="danger float-left" onClick={handleCloseViewActions}>
-                        Close
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+                <TableWrapper title="Action Lists" columns={actionColumns} data={actions} options={{ pageSize: 10 }} />
+            </ModalWrapper>
         </>
     );
 }
