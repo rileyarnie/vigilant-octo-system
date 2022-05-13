4516a8c745cea247953f94427c0c57b16303229c
David Kiragu <davidkmuriba@gmail.com>
2022-05-13 19:27:33 +0000

    Merged in SILMS-1584-add-confirmation-modal-on-add_action_UI (pull request #218)
    SILMS-1584 add confirmation modal on add action UI
    
    * Fixed update object to send correct data
    
    * Added conformation Modal on add action modal
    
    * updated to close modal on .finally
    
    * Removed unused imports
    
    * Merge remote-tracking branch 'origin/develop' into SILMS-1584-add-confirmation-modal-on-add_action_UI
    
    # Conflicts:
    #	src/pages/Users/AddActionsModal/AddActionsModal.tsx
    
    * Fixed conflicts
    
    
    Approved-by: Brian Gacheru
    Approved-by: Arnold Kibira

--- src/pages/Users/AddActionsModal/AddActionsModal.tsx
+++ src/pages/Users/AddActionsModal/AddActionsModal.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
-import { Alerts, ToastifyAlerts } from '../../lib/Alert';
-import { authnzAxiosInstance } from '../../../utlis/interceptors/authnz-interceptor';
+import React, {useEffect, useState} from 'react';
+import {Button, Modal} from 'react-bootstrap';
+import {Alerts, ToastifyAlerts} from '../../lib/Alert';
+import {authnzAxiosInstance} from '../../../utlis/interceptors/authnz-interceptor';
 import TableWrapper from '../../../utlis/TableWrapper';
+import ConfirmationModalWrapper from '../../../App/components/modal/ConfirmationModalWrapper';
+
 const alerts: Alerts = new ToastifyAlerts();
 
 interface ISelectedRowProps {
@@ -30,6 +32,8 @@ export const AddActionsModal = (props: IProps): JSX.Element => {
     const [selectedRows, setSelectedRows] = useState([]);
 
     const options: IOptions[] = [];
+    const [disabled, setDisabled] = useState(false);
+    const [confirmModal, setConfirmModal] = useState(false);
     useEffect(() => {
         authnzAxiosInstance
             .get('/actions')
@@ -43,12 +47,12 @@ export const AddActionsModal = (props: IProps): JSX.Element => {
     }, []);
 
     const columns = [
-        { title: 'ID', field: 'id', hidden: false },
-        { title: 'Name', field: 'name' },
-        { title: 'Description', field: 'description' }
+        {title: 'ID', field: 'id', hidden: false},
+        {title: 'Name', field: 'name'},
+        {title: 'Description', field: 'description'}
     ];
     actions.map((action) => {
-        return options.push({ value: action.id, label: action.name + ' - ' + action.description });
+        return options.push({value: action.id, label: action.name + ' - ' + action.description});
     });
 
     const getDefaultChecked = (data, defaultValuesId) => {
@@ -59,7 +63,7 @@ export const AddActionsModal = (props: IProps): JSX.Element => {
         for (let i = 0; i < data.length; i++) {
             const element = data[i];
             if (defaultValuesId.indexOf(element.id) > -1) {
-                defaultChecked.push({ ...element, tableData: { checked: true } });
+                defaultChecked.push({...element, tableData: {checked: true}});
             } else {
                 defaultChecked.push(element);
             }
@@ -78,19 +82,19 @@ export const AddActionsModal = (props: IProps): JSX.Element => {
     const handlePostRoles = async () => {
         const roleId = props.selectedRowProps.id;
         authnzAxiosInstance
-            .post(`/roles/${roleId}/actions`, { actionID: selectedRows })
+            .post(`/roles/${roleId}/actions`, {actionID: selectedRows})
             .then((res) => {
                 if (res.status == 200) {
                     alerts.showSuccess('Successfully assigned actions to role');
                 }
             })
             .catch((error) => {
-                //handle error using logging library
-                console.error(error);
                 alerts.showError(error.message);
             })
             .finally(() => {
                 props.onHide();
+                setDisabled(false);
+                toggleCloseConfirmModal();
             });
     };
 
@@ -100,32 +104,51 @@ export const AddActionsModal = (props: IProps): JSX.Element => {
         setSelectedRows(uniq);
     };
 
+    const toggleConfirmModal = () => {
+        setConfirmModal(true);
+    };
+    const toggleCloseConfirmModal = () => {
+        setConfirmModal(false);
+    };
     return (
-        <Modal {...props} size="lg" backdrop="static" aria-labelledby="contained-modal-title-vcenter" centered>
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">Assign actions to {props.selectedRowProps.name}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <TableWrapper
-                    title={`Assign actions to ${props.selectedRowProps.name}`}
-                    columns={columns}
-                    data={defaultCheckedRows}
-                    options={{
-                        selection: true,
-                        showSelectAllCheckbox: false,
-                        showTextRowsSelected: false
-                    }}
-                    onSelectionChange={(rows) => handleRowSelection(rows)}
-                />
-            </Modal.Body>
-            <Modal.Footer style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Button variant="danger" onClick={props.toggleModal}>
-                    Cancel
-                </Button>
-                <Button variant="primary" onClick={handlePostRoles}>
-                    Save
-                </Button>
-            </Modal.Footer>
-        </Modal>
+        <>
+            <Modal {...props} size="lg" backdrop="static" aria-labelledby="contained-modal-title-vcenter" centered>
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">Assign actions
+                        to {props.selectedRowProps.name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <TableWrapper
+                        title={`Assign actions to ${props.selectedRowProps.name}`}
+                        columns={columns}
+                        data={defaultCheckedRows}
+                        options={{
+                            selection: true,
+                            showSelectAllCheckbox: false,
+                            showTextRowsSelected: false
+                        }}
+                        onSelectionChange={(rows) => handleRowSelection(rows)}
+                    />
+                </Modal.Body>
+                <Modal.Footer style={{display: 'flex', justifyContent: 'space-between'}}>
+                    <Button variant="danger" onClick={props.toggleModal}>
+                        Cancel
+                    </Button>
+                    <Button variant="primary" onClick={toggleConfirmModal}>
+                        Save
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            <ConfirmationModalWrapper
+                disabled={disabled}
+                submitButton
+                submitFunction={handlePostRoles}
+                closeModal={toggleCloseConfirmModal}
+                show={confirmModal}
+            >
+                <h6 className="text-center">Are you sure you want to Assign actions
+                    to {props.selectedRowProps.name} ?</h6>
+            </ConfirmationModalWrapper>
+        </>
     );
 };
