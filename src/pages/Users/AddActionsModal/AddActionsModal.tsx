d0c6f16be86ec50db76bcc85cd9bca7519ac0aa9
Arnold Kibira <arnold.ril3y@gmail.com>
2022-05-13 18:29:02 +0000

    Merged in SILMS-1484-staff-portal-roles-assigning- (pull request #221)
    SILMS-1484 staff portal roles assigning
    
    * modaify modal to have table with checkboxes and have the already assign roles checked
    
    * add data to the dependency array
    
    
    Approved-by: David Kiragu

--- src/pages/Users/AddActionsModal/AddActionsModal.tsx
+++ src/pages/Users/AddActionsModal/AddActionsModal.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Modal, Button } from 'react-bootstrap';
-import Select from 'react-select';
 import { Alerts, ToastifyAlerts } from '../../lib/Alert';
-import { customSelectTheme } from '../../lib/SelectThemes';
 import { authnzAxiosInstance } from '../../../utlis/interceptors/authnz-interceptor';
+import TableWrapper from '../../../utlis/TableWrapper';
 const alerts: Alerts = new ToastifyAlerts();
-
 
 interface ISelectedRowProps {
     name: string;
@@ -27,8 +25,10 @@ interface IProps {
 
 export const AddActionsModal = (props: IProps): JSX.Element => {
     const [actions, setActions] = useState([]);
-    const [isMulti] = useState(true);
-    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [defaultValuesId, setDefaultValuesId] = useState([]);
+    const [defaultCheckedRows, setDefaultCheckedRows] = useState([]);
+    const [selectedRows, setSelectedRows] = useState([]);
+
     const options: IOptions[] = [];
     useEffect(() => {
         authnzAxiosInstance
@@ -42,34 +42,62 @@ export const AddActionsModal = (props: IProps): JSX.Element => {
             });
     }, []);
 
+    const columns = [
+        { title: 'ID', field: 'id', hidden: false },
+        { title: 'Name', field: 'name' },
+        { title: 'Description', field: 'description' }
+    ];
     actions.map((action) => {
-        return options.push({ value: action.id, label: action.name+ ' - '+ action.description});
+        return options.push({ value: action.id, label: action.name + ' - ' + action.description });
     });
-    const handleChange = (selectedOptions) => {
-        setSelectedOptions(selectedOptions);
+
+    const getDefaultChecked = (data, defaultValuesId) => {
+        const defaultChecked = [];
+        if (data.length <= 0 || defaultValuesId <= 0) {
+            return;
+        }
+        for (let i = 0; i < data.length; i++) {
+            const element = data[i];
+            if (defaultValuesId.indexOf(element.id) > -1) {
+                defaultChecked.push({ ...element, tableData: { checked: true } });
+            } else {
+                defaultChecked.push(element);
+            }
+        }
+        return setDefaultCheckedRows(defaultChecked);
     };
 
+    useEffect(() => {
+        getDefaultChecked(actions, defaultValuesId);
+    }, [actions, defaultValuesId]);
+
+    useEffect(() => {
+        setDefaultValuesId(props.defaultValues.map((item) => item.value));
+    }, [actions, props.defaultValues]);
+
     const handlePostRoles = async () => {
-        const actionsArr = [];
         const roleId = props.selectedRowProps.id;
-        selectedOptions.map((option) => {
-            return actionsArr.push(option.value);
-        });
         authnzAxiosInstance
-            .post(`/roles/${roleId}/actions`, { actionID: actionsArr })
+            .post(`/roles/${roleId}/actions`, { actionID: selectedRows })
             .then((res) => {
                 if (res.status == 200) {
                     alerts.showSuccess('Successfully assigned actions to role');
-                    console.log(res);
-                    props.onHide();
                 }
             })
             .catch((error) => {
                 //handle error using logging library
                 console.error(error);
                 alerts.showError(error.message);
+            })
+            .finally(() => {
                 props.onHide();
             });
+    };
+
+    const handleRowSelection = (rows) => {
+        const roleIds = rows.map((row) => row.id);
+        const uniq = [...new Set(roleIds)];
+        setSelectedRows(uniq);
     };
 
     return (
@@ -78,14 +106,16 @@ export const AddActionsModal = (props: IProps): JSX.Element => {
                 <Modal.Title id="contained-modal-title-vcenter">Assign actions to {props.selectedRowProps.name}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Select
-                    defaultValue={props.defaultValues}
-                    theme={customSelectTheme}
-                    options={options}
-                    isMulti={isMulti}
-                    placeholder="Select Actions for this Role"
-                    noOptionsMessage={() => 'No available actions'}
-                    onChange={handleChange}
+                <TableWrapper
+                    title={`Assign actions to ${props.selectedRowProps.name}`}
+                    columns={columns}
+                    data={defaultCheckedRows}
+                    options={{
+                        selection: true,
+                        showSelectAllCheckbox: false,
+                        showTextRowsSelected: false
+                    }}
+                    onSelectionChange={(rows) => handleRowSelection(rows)}
                 />
             </Modal.Body>
             <Modal.Footer style={{ display: 'flex', justifyContent: 'space-between' }}>
