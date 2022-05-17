eb4483f292645c0573a4818e766f67141cf3e839
David Kiragu <davidkmuriba@gmail.com>
2022-05-17 15:22:26 +0300

    Updated view application modal to show all details Removed redundant data in ApplicationsList.tsx

--- src/pages/Academics/ApplicationsList.tsx
+++ src/pages/Academics/ApplicationsList.tsx
@@ -6,19 +6,47 @@ import { Link } from 'react-router-dom';
 import Alert from '@material-ui/lab/Alert';
 import Breadcrumb from '../../App/components/Breadcrumb';
 import { Button, Card, Col, ListGroup, Modal, Row } from 'react-bootstrap';
-import { FileInput, ValidationForm } from 'react-bootstrap4-form-validation';
 import { Alerts, ToastifyAlerts } from '../lib/Alert';
 import LinearProgress from '@mui/material/LinearProgress';
-import { TimetableService } from '../../services/TimetableService';
 import { canPerformActions } from '../../services/ActionChecker';
 import { ACTION_GET_PROGRAM_COHORT_APPLICATIONS } from '../../authnz-library/sim-actions';
 import { simsAxiosInstance } from '../../utlis/interceptors/sims-interceptor';
-import { timetablingAxiosInstance } from '../../utlis/interceptors/timetabling-interceptor';
 import TableWrapper from '../../utlis/TableWrapper';
 import EditApplicationDetails from './Application/EditApplicationDetails';
 
 const alerts: Alerts = new ToastifyAlerts();
 const ApplicationsList = (): JSX.Element => {
+    interface campus {
+        name: string,
+        id: number
+    }
+    interface applicationDetails {
+        applications_firstName: string,
+        applications_lastName: string,
+        applications_otherName: string,
+        applications_identification: string,
+        applications_gender: string,
+        applications_campus: number,
+        applications_countyOfResidence: string,
+        applications_dateOfBirth: string,
+        applications_emailAddress: string,
+        applications_identificationType: string,
+        applications_maritalStatus: string,
+        applications_nationality: string,
+        applications_phoneNumber: string,
+        applications_physicalChallenges: string,
+        applications_physicalChallengesDetails: string,
+        applications_preferredStartDate: string,
+        applications_religion: string,
+        applications_sponsor: string,
+        applications_programCohortId: string,
+        programCohortId: number,
+        nkd_name: string,
+        nkd_nextOfKinPhoneNumber: string,
+        nkd_relation: string,
+        campus: campus,
+        sdocs_documentUrl: string
+    }
     const columns = [
         { title: 'ID', field: 'applications_id' },
         { title: 'Name', render: (rowData) => rowData.applications_firstName + ' ' + rowData.applications_lastName },
@@ -40,32 +68,8 @@ const ApplicationsList = (): JSX.Element => {
     const [linearDisplay, setLinearDisplay] = useState('none');
     const [isAdmitted, setIsAdmitted] = useState('PENDING');
     const [selectedRow] = useState(null);
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [otherName, setOtherName] = useState('');
-    const [nationality, setNationality] = useState('');
-    const [identification, setIdentification] = useState('');
-    const [gender, setGender] = useState('');
-    const [maritalStatus, setMaritalStatus] = useState('');
-    const [religion, setReligion] = useState('');
-    const [dateOfBirth, setDateOfBirth] = useState('');
-    const [placeofBirth, setPlaceOfBirth] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [emailAddress, setEmailAddress] = useState('');
-    const [nextOfKinName, setNextOfKinName] = useState('');
-    const [nextOfKinPhoneNumber, setNextOfKinPhoneNumber] = useState('');
-    const [nextOfKinRelation, setNextOfKinRelation] = useState('');
-    const [physicalChallenges, setPhysicalChallenges] = useState('');
-    const [courseStartDate, setCourseStartDate] = useState('');
-    const [campus, setCampus] = useState('');
-    const [fileUploaded, setFileUploaded] = useState('');
-    const [programCohortId, setProgramCohortId] = useState('');
-    const [sponsor, setSponsor] = useState('');
-    const [countryOfResidence, setCountryOfResidence] = useState('');
-    const [documentsUrl, setDocumentsUrl] = useState('');
-    const [applicationData, setApplicationData] = useState([]);
+    const [applicationData, setApplicationData] = useState<applicationDetails>();
     const [applicationId, setApplicationId] = useState('');
-    const [showUploadModal, setShowUploadModal] = useState(false);
     const [disabled, setDisabled] = useState(false);
 
     useEffect(() => {
@@ -86,27 +90,6 @@ const ApplicationsList = (): JSX.Element => {
                 setLinearDisplay('none');
             });
     };
-    function handleUpload() {
-        const form = new FormData();
-        setLinearDisplay('block');
-        form.append('fileUploaded', fileUploaded);
-        const config = {
-            headers: { 'content-type': 'multipart/form-data' }
-        };
-        TimetableService.handleFileUpload(form, config);
-        timetablingAxiosInstance
-            .post('/files', form, config)
-            .then((res) => {
-                toggleUploadModal();
-                alerts.showSuccess('File uploaded successfully');
-                setDocumentsUrl(res.data);
-                setLinearDisplay('none');
-            })
-            .catch((error) => {
-                alerts.showError(error.message);
-                setLinearDisplay('none');
-            });
-    }
     const handleAdmission = (e, admissionStatus: admissionStatus) => {
         e.preventDefault();
         const message = admissionStatus === 'ADMITTED' ? 'The application has been accepted' : 'The application has been rejected';
@@ -144,10 +127,6 @@ const ApplicationsList = (): JSX.Element => {
     const handleCloseModal = () => {
         modalShow ? resetStateCloseModal() : setModalShow(false);
     };
-    const toggleUploadModal = () => {
-        showUploadModal ? setShowUploadModal(false) : setShowUploadModal(true);
-    };
-
     return (
         <>
             <Row className="align-items-center page-header">
@@ -176,33 +155,10 @@ const ApplicationsList = (): JSX.Element => {
                                     onRowClick={(event, row) => {
                                         toggleCreateModal();
                                         setApplicationData(row);
-                                        setFirstName(row.applications_firstName);
-                                        setLastName(row.applications_lastName);
-                                        setOtherName(row.applications_otherName);
-                                        setIdentification(row.applications_identification);
-                                        setGender(row.applications_gender);
-                                        setMaritalStatus(row.applications_maritalStatus);
-                                        setReligion(row.applications_religion);
-                                        setDateOfBirth(row?.applications_dateOfBirth?.slice(0, 10));
-                                        setPlaceOfBirth(row.applications_placeofBirth);
-                                        setPhoneNumber(row.applications_phoneNumber);
-                                        setEmailAddress(row.applications_emailAddress);
-                                        setNationality(row.applications_nationality);
-                                        setPhysicalChallenges(row.applications_physicalChallenges);
-                                        setCourseStartDate(row?.applications_courseStartDate?.slice(0, 10));
-                                        setCampus(row.applications_campus);
-                                        setSponsor(row.applications_sponsor);
-                                        setCountryOfResidence(row.applications_countryOfResidence);
-                                        setProgramCohortId(row.applications_programCohortId);
                                         setIsAdmitted(row.applications_status);
-                                        setNextOfKinName(row.nkd_name);
-                                        setNextOfKinPhoneNumber(row.nkd_nextOfKinPhoneNumber);
-                                        setNextOfKinRelation(row.nkd_relation);
                                         setApplicationId(row.applications_id);
-                                        setDocumentsUrl(row.sdocs_documentUrl);
                                     }}
                                     data={data}
-                                    //
                                     options={{
                                         rowStyle: (rowData) => ({
                                             backgroundColor: selectedRow === rowData.tableData.id ? '#EEE' : '#FFF'
@@ -248,7 +204,7 @@ const ApplicationsList = (): JSX.Element => {
                         Application Id: {applicationId}
                         {isAdmitted === 'ADMITTED' && (
                             <>
-                                <Link to={'/publishedsemesters'} onClick={() => localStorage.setItem('programId', programCohortId)}>
+                                <Link to={'/publishedsemesters'} onClick={() => localStorage.setItem('programId', applicationData?.applications_programCohortId)}>
                                     <Button style={{ marginRight: '.5rem', marginLeft: '.5rem' }} variant="info" onClick={handleClose}>
                                         View Semesters
                                     </Button>
@@ -291,27 +247,28 @@ const ApplicationsList = (): JSX.Element => {
                     <Row>
                         <div className="col-md-6">
                             <ListGroup>
-                                <ListGroup.Item>First Name: {firstName}</ListGroup.Item>
-                                <ListGroup.Item>Last Name: {lastName}</ListGroup.Item>
-                                <ListGroup.Item>Other Name: {otherName}</ListGroup.Item>
-                                <ListGroup.Item>Nationality: {nationality}</ListGroup.Item>
-                                <ListGroup.Item>Identification: {identification}</ListGroup.Item>
-                                <ListGroup.Item>Gender: {gender}</ListGroup.Item>
-                                <ListGroup.Item>Marital Status: {maritalStatus}</ListGroup.Item>
-                                <ListGroup.Item>Religion: {religion}</ListGroup.Item>
-                                <ListGroup.Item>Date Of Birth: {dateOfBirth}</ListGroup.Item>
+                                <ListGroup.Item>First Name: {applicationData?.applications_firstName}</ListGroup.Item>
+                                <ListGroup.Item>Last Name: {applicationData?.applications_lastName}</ListGroup.Item>
+                                <ListGroup.Item>Other Name: {applicationData?.applications_otherName}</ListGroup.Item>
+                                <ListGroup.Item>Nationality: {applicationData?.applications_nationality}</ListGroup.Item>
+                                <ListGroup.Item>Identification Type: {applicationData?.applications_identificationType}</ListGroup.Item>
+                                <ListGroup.Item>Identification No: {applicationData?.applications_identification}</ListGroup.Item>
+                                <ListGroup.Item>Gender: {applicationData?.applications_gender}</ListGroup.Item>
+                                <ListGroup.Item>Marital Status: {applicationData?.applications_maritalStatus}</ListGroup.Item>
+                                <ListGroup.Item>Religion: {applicationData?.applications_religion}</ListGroup.Item>
+                                <ListGroup.Item>Date Of Birth: {applicationData?.applications_dateOfBirth?.slice(0,10)}</ListGroup.Item>
                             </ListGroup>
                         </div>
                         <div className="col-md-6">
                             <ListGroup>
-                                <ListGroup.Item>Place of Birth: {placeofBirth}</ListGroup.Item>
-                                <ListGroup.Item>Phone Number: {phoneNumber}</ListGroup.Item>
-                                <ListGroup.Item>Email Address: {emailAddress}</ListGroup.Item>
-                                <ListGroup.Item>Physical Challenges: {physicalChallenges}</ListGroup.Item>
-                                <ListGroup.Item>Course Start Date: {courseStartDate}</ListGroup.Item>
-                                <ListGroup.Item>Campus: {campus}</ListGroup.Item>
-                                <ListGroup.Item>Sponsor: {sponsor}</ListGroup.Item>
-                                <ListGroup.Item>Country Of Residence: {countryOfResidence}</ListGroup.Item>
+                                <ListGroup.Item>Phone Number: {applicationData?.applications_phoneNumber}</ListGroup.Item>
+                                <ListGroup.Item>Email Address: {applicationData?.applications_emailAddress}</ListGroup.Item>
+                                <ListGroup.Item>Physical Challenges: {applicationData?.applications_physicalChallenges}</ListGroup.Item>
+                                <ListGroup.Item>Details: {applicationData?.applications_physicalChallengesDetails}</ListGroup.Item>
+                                <ListGroup.Item>Campus: {applicationData?.applications_campus}</ListGroup.Item>
+                                <ListGroup.Item>Preferred Start Date: {applicationData?.applications_preferredStartDate?.slice(0,10)}</ListGroup.Item>
+                                <ListGroup.Item>Sponsor: {applicationData?.applications_sponsor}</ListGroup.Item>
+                                <ListGroup.Item>County Of Residence: {applicationData?.applications_countyOfResidence}</ListGroup.Item>
                             </ListGroup>
                         </div>
                     </Row>
@@ -320,14 +277,14 @@ const ApplicationsList = (): JSX.Element => {
                         <div className="col-md-6">
                             <h6>Next of Kin Details</h6>
                             <ListGroup>
-                                <ListGroup.Item>Name: {nextOfKinName}</ListGroup.Item>
-                                <ListGroup.Item>Phone Number: {nextOfKinPhoneNumber}</ListGroup.Item>
-                                <ListGroup.Item>Relation: {nextOfKinRelation}</ListGroup.Item>
+                                <ListGroup.Item>Name: {applicationData?.nkd_name}</ListGroup.Item>
+                                <ListGroup.Item>Phone Number: {applicationData?.nkd_nextOfKinPhoneNumber}</ListGroup.Item>
+                                <ListGroup.Item>Relation: {applicationData?.nkd_relation}</ListGroup.Item>
                             </ListGroup>
                         </div>
                         <div className="col-md-6">
                             <ListGroup>
-                                <ListGroup.Item>Document Url: {documentsUrl}</ListGroup.Item>
+                                <ListGroup.Item>Document Url: {applicationData?.sdocs_documentUrl}</ListGroup.Item>
                             </ListGroup>
                         </div>
                     </Row>
@@ -354,51 +311,6 @@ const ApplicationsList = (): JSX.Element => {
                 <Modal.Body>
                     <EditApplicationDetails application={applicationData} close={resetStateCloseModal} />
                 </Modal.Body>
-            </Modal>
-            <Modal
-                backdrop="static"
-                show={showUploadModal}
-                onHide={toggleUploadModal}
-                size="sm"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Dialog>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Upload document</Modal.Title>
-                    </Modal.Header>
-
-                    <Modal.Body>
-                        <ValidationForm>
-                            <FileInput
-                                name="fileUploaded"
-                                id="image"
-                                encType="multipart/form-data"
-                                fileType={['pdf', 'doc', 'docx']}
-                                maxFileSize="10mb"
-                                onInput={(e) => {
-                                    setFileUploaded(() => {
-                                        return e.target.files[0];
-                                    });
-                                }}
-                                errorMessage={{
-                                    required: 'Please upload a document',
-                                    fileType: 'Only document is allowed',
-                                    maxFileSize: 'Max file size is 10MB'
-                                }}
-                            />
-                        </ValidationForm>
-                    </Modal.Body>
-
-                    <Modal.Footer style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Button variant="primary" className="btn btn-primary rounded" onClick={toggleUploadModal} disabled={disabled}>
-                            Close
-                        </Button>
-                        <Button variant="danger" className="btn btn-danger rounded" onClick={() => handleUpload()} disabled={disabled}>
-                            Upload
-                        </Button>
-                    </Modal.Footer>
-                </Modal.Dialog>
             </Modal>
         </>
     );
