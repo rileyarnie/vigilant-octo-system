318a55879129e91d6f58ebcff234ae7f016e06a1
Arnold Kibira <arnold.ril3y@gmail.com>
2022-05-27 01:32:22 +0300

    show student details in transactions details

--- src/pages/finance/Transactions.tsx
+++ src/pages/finance/Transactions.tsx
@@ -304,11 +304,12 @@ const Transactions = (): JSX.Element => {
                 axios.spread((...responses) => {
                     const staff = responses[0] && responses[0].data;
                     setRecordedBy({ staffId: staff.id, name: staff.name });
-                    const studentCrFirstName = responses[1] && responses[1].data.applications_firstName;
-                    const studentCrLastName = responses[1] && responses[1].data.applications_lastName;
+                    console.log('responses[1].data[0]', responses[1].data[0]);
+                    const studentCrFirstName = responses[1] && responses[1]?.data[0].applications_firstName;
+                    const studentCrLastName = responses[1] && responses[1]?.data[0].applications_lastName;
                     setStudentNameCr(`${studentCrFirstName} ${studentCrLastName}`);
-                    const studentDrFirstName = responses[2] && responses[2].data.applications_firstName;
-                    const studentDrLastName = responses[2] && responses[2].data.applications_lastName;
+                    const studentDrFirstName = responses[2] && responses[2].data[0]?.applications_firstName;
+                    const studentDrLastName = responses[2] && responses[2].data[0]?.applications_lastName;
                     setStudentNameDr(`${studentDrFirstName} ${studentDrLastName}`);
                     const balanceCr = responses[3] && responses[3].data.balance;
                     setFeeBalanceCr(balanceCr);
