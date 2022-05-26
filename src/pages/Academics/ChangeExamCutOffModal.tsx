19f078f6ac6eee49034473503ce1b0ac99cf916a
Arnold Kibira <arnold.ril3y@gmail.com>
2022-05-26 22:15:07 +0300

    refactor modal to be design pattern and use consistent form

--- src/pages/Academics/ChangeExamCutOffModal.tsx
+++ src/pages/Academics/ChangeExamCutOffModal.tsx
@@ -1,94 +1,99 @@
 import React, { useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import ConfirmationModalWrapper from '../../App/components/modal/ConfirmationModalWrapper';
+import ModalWrapper from '../../App/components/modal/ModalWrapper';
 import { timetablingAxiosInstance } from '../../utlis/interceptors/timetabling-interceptor';
-import {Alerts, ToastifyAlerts} from '../lib/Alert';
+import { Alerts, ToastifyAlerts } from '../lib/Alert';
+import { TextInput, ValidationForm } from 'react-bootstrap4-form-validation';
 
 const alerts: Alerts = new ToastifyAlerts();
 
-const ChangeExamCutOffModal: React.FunctionComponent<{
-    programCohortSemesterId:number,
-     showCutOffModal:boolean, 
-     setShowModal,
-     modalTitle:string
-    }> = (props) => {
+interface Props {
+    programCohortSemesterId: number;
+    showCutOffModal: boolean;
+    setShowModal;
+    modalTitle: string;
+    examCutOffDate: Date | null;
+}
 
-        const [cutOffDate, setCutOffDate] = useState(new Date());
-        const [disabled, setDisabled] = useState(false);
-        const [activationModal, setActivationModal] = useState(false);
-        const handleClose = () => props.setShowModal(false); 
-        function handleCloseActivationModal () {
+const ChangeExamCutOffModal: React.FunctionComponent<Props> = (props) => {
+    const [cutOffDate, setCutOffDate] = useState(new Date());
+    const [disabled, setDisabled] = useState(false);
+    const [activationModal, setActivationModal] = useState(false);
+    const handleClose = () => props.setShowModal(false);
+    function handleCloseActivationModal() {
+        setActivationModal(false);
+    }
+
+    async function updatePCSCutOffDate(pcsId: number, updates: unknown) {
+        setDisabled(true);
+        try {
+            try {
+                await timetablingAxiosInstance.put(`/program-cohort-semesters/${pcsId}/marks-entry-cutoff`, updates);
+                alerts.showSuccess('Successfully updated program cohort semester');
+                props.setShowModal(false);
+            } catch (error) {
+                alerts.showError((error as Error).message);
+            }
+        } finally {
+            setDisabled(false);
             setActivationModal(false);
         }
+    }
 
-        async function updatePCSCutOffDate(pcsId: number, updates:unknown){
-            setDisabled(true);
-            try {
-                try {
-                    await timetablingAxiosInstance
-                        .put(`/program-cohort-semesters/${pcsId}/marks-entry-cutoff`, updates);
-                    alerts.showSuccess('Successfully updated program cohort semester');
-                } catch (error) {
-                    alerts.showError((error as Error).message);
-                }
-            } finally {
-                setDisabled(false);
-                setActivationModal(false);
-                props.setShowModal(false);
-            }
-        }
-    
-        return (
-            <>
-  
-                <Modal
-                    show={props.showCutOffModal}
-                    onHide={handleClose}
-                    backdrop="static"
-                    keyboard={false}
-                    centered
+    return (
+        <>
+            <ModalWrapper show={props.showCutOffModal} title={props.modalTitle} closeModal={handleClose} modalSize="md" noFooter>
+                <h6>Enter a new cut off date</h6>
+                <ValidationForm
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        setActivationModal(true);
+                    }}
                 >
-                    <Modal.Header closeButton>
-                        <Modal.Title>{props.modalTitle} </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <p>Enter a new cut off date</p>
-                        <div style={{display: 'flex', justifyContent: 'center'}}>
-                            <div>
-                                <DatePicker selected={cutOffDate} onChange={(date:Date) => setCutOffDate(date)} />
-                            </div>
-                        </div>
-                    </Modal.Body>
-                    <Modal.Footer style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Button disabled={disabled} variant="btn btn-info btn-rounded" onClick={handleClose}>
-                        Close
-                        </Button>
-                        <Button
-                            disabled={disabled}
-                            onClick={() => {
-                                setActivationModal(true);
+                    <div>
+                        <TextInput
+                            name="startDate"
+                            id="startDate"
+                            type="date"
+                            required
+                            defaultValue={props.examCutOffDate ? props.examCutOffDate : new Date()}
+                            onChange={(e) => {
+                                setCutOffDate(e.target.value);
                             }}
-                            variant="btn btn-danger btn-rounded"
+                        />
+                    </div>
+                    <div className="form-group mt-4">
+                        <button className="btn btn-info float-right">Submit</button>
+                        <button
+                            className="btn btn-danger float-left"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                handleClose();
+                            }}
                         >
-                        Submit
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
+                            Cancel
+                        </button>
+                    </div>
+                </ValidationForm>
+            </ModalWrapper>
 
-                <ConfirmationModalWrapper
-                    disabled={disabled}
-                    submitButton
-                    submitFunction={() => updatePCSCutOffDate(props.programCohortSemesterId,{examCutOffDate:cutOffDate})}
-                    closeModal={handleCloseActivationModal}
-                    show={activationModal}
-                >
-                    <h6 className="text-center">
-                                Are you sure you want to change the exam cut off date of Program Cohort Semester Id {props.programCohortSemesterId} to {cutOffDate.toDateString()}
-                    </h6>
-                </ConfirmationModalWrapper>
-            </>
-        );
-    };
+            <ConfirmationModalWrapper
+                disabled={disabled}
+                submitButton
+                submitFunction={() =>
+                    updatePCSCutOffDate(props.programCohortSemesterId, { examCutOffDate: new Date(cutOffDate).toISOString() })
+                }
+                closeModal={handleCloseActivationModal}
+                show={activationModal}
+            >
+                <h6 className="text-center">
+                    Are you sure you want to change the exam cut off date of Program Cohort Semester Id {props.programCohortSemesterId} to{' '}
+                    {new Date(cutOffDate).toDateString()}
+                </h6>
+            </ConfirmationModalWrapper>
+        </>
+    );
+};
 
 export default ChangeExamCutOffModal;
